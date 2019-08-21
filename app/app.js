2ddd13d27d3b1da98677826ad5650b754b4440f0
Abraham, Jakob <jakob.abraham@baloise.ch>
2019-08-21 08:53:06 +0200

    Removed Register-Elements

--- app/app.js
+++ app/app.js
@@ -1,39 +1,10 @@
 var app = angular.module("multimediaApp", []);
 app.controller("authController", function ($scope, $http) {
-    $scope.closeMsg = function () {
-        $scope.alertMsg = fasle;
-    };
 
     $scope.loginForm = true;
 
-    $scope.showRegister = function () {
-        $scope.loginForm = false;
-        $scope.registerForm = true;
-        $scope.alertMsg = false;
-    };
-
     $scope.showLogin = function () {
-        $scope.registerForm = false;
         $scope.loginForm = true;
-        $scope.alertMsg = false;
-    };
-
-    $scope.submitRegister = function () {
-        $hhtp({
-            method: "POST",
-            url: "app/api/register.php",
-            data: $scope.registerData
-        }).then(function (data) {
-            $scope.alertMsg = true;
-            if (data.error != '') {
-                $scope.alertClass = 'alert-danger';
-                $scope.alertMessage = data.error;
-            } else {
-                $scope.alertClass = 'alert-success';
-                $scoope.alertMessage = data.message;
-                $scope.registerData = {};
-            }
-        });
     };
 
     $scope.submitLogin = function () {
